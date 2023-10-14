f041350b12dc7cf95f4f141840254a6fc384134b
Qeiqkaos <subanov.oleg@mail.ru>
2023-10-13 21:14:00 -0400

    The part of main page

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    return render(request, "app/base.html")
+    return render(request, "app/main.html")
 
 
 def service(request):
