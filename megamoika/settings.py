70a38a91e5e4e1ccce6babcff5c253857562d924
Qeiqkaos <subanov.oleg@mail.ru>
2024-01-12 23:28:48 -0500

    tired

--- megamoika/settings.py
+++ megamoika/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'app\static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'app\static')
 
 
 # Default primary key field type
